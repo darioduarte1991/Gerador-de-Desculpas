52ed0ed3d02723d103935d85c9adb9b1732fb3a7
darioduarte1991 <162734794+darioduarte1991@users.noreply.github.com>
2024-04-21 16:02:50 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function excuseGenerator() {
+  
+  let numWho=0;
+  let numAction=0;
+  let numWhat=0;
+  let numWhen=0;
+  
+  numWho = Math.floor(Math.random() * who.length);
+  numAction = Math.floor(Math.random() * action.length);
+  numWhat = Math.floor(Math.random() * what.length);
+  numWhen = Math.floor(Math.random() * when.length);
+  
+  return who[numWho] + " " + action[numAction] + " " + what[numWhat] + " " + when[numWhen];
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function onLoad(){
+  excuse = document.getElementById('excuse');
+
+}
