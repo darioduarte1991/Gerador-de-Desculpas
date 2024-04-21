cd2b896fa00f2ec5e0c136a7acd916718d00e57e
darioduarte1991 <162734794+darioduarte1991@users.noreply.github.com>
2024-04-21 17:02:48 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,24 +1,38 @@
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
 
 function excuseGenerator() {
-  
-  let numWho=0;
-  let numAction=0;
-  let numWhat=0;
-  let numWhen=0;
-  
+  let numWho = 0;
+  let numAction = 0;
+  let numWhat = 0;
+  let numWhen = 0;
+
   numWho = Math.floor(Math.random() * who.length);
   numAction = Math.floor(Math.random() * action.length);
   numWhat = Math.floor(Math.random() * what.length);
   numWhen = Math.floor(Math.random() * when.length);
-  
-  return who[numWho] + " " + action[numAction] + " " + what[numWhat] + " " + when[numWhen];
+
+  return (
+    who[numWho] +
+    " " +
+    action[numAction] +
+    " " +
+    what[numWhat] +
+    " " +
+    when[numWhen]
+  );
 }
 
-function onLoad(){
-  excuse = document.getElementById('excuse');
-
+function onLoad() {
+  const excuse = document.getElementById("excuse");
+  excuse.innerHTML = excuseGenerator(who, action, what, when);
 }
+document.addEventListener("DOMContentLoaded", onLoad);
